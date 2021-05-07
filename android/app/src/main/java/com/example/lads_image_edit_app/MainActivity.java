81c90cfb2556c8a7e6eb3d90248ba37868fc1ae9
Mufazal <mufazalahmad2002@gmail.com>
2021-05-07 10:45:32 +0500

    change apply

--- android/app/src/main/java/com/example/lads_image_edit_app/MainActivity.java
+++ android/app/src/main/java/com/example/lads_image_edit_app/MainActivity.java
@@ -1,11 +1,41 @@
 package com.example.lads_image_edit_app;
 
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
 import java.io.File;
+import java.nio.MappedByteBuffer;
 
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+    private static final String TAG = "SuperResolution";
+    private static final String MODEL_NAME = "ESRGAN.tflite";
+    private static final int LR_IMAGE_HEIGHT = 50;
+    private static final int LR_IMAGE_WIDTH = 50;
+    private static final int UPSCALE_FACTOR = 4;
+    private static final int SR_IMAGE_HEIGHT = LR_IMAGE_HEIGHT * UPSCALE_FACTOR;
+    private static final int SR_IMAGE_WIDTH = LR_IMAGE_WIDTH * UPSCALE_FACTOR;
+    private static final String LR_IMG_1 = "lr-1.jpg";
+    private static final String LR_IMG_2 = "lr-2.jpg";
+    private static final String LR_IMG_3 = "lr-3.jpg";
 
+    private MappedByteBuffer model;
+    private long superResolutionNativeHandle = 0;
+    private Bitmap selectedLRBitmap = null;
+    private boolean useGPU = false;
+
+    private ImageView lowResImageView1;
+    private ImageView lowResImageView2;
+    private ImageView lowResImageView3;
+    private TextView selectedImageTextView;
+    private Switch gpuSwitch;
     File imageFile;
     private static final String CHANNEL = "sample.resolution.dev/image";
 
@@ -35,11 +65,7 @@ public class MainActivity extends FlutterActivity {
         return "HI";
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
 
-    }
 
     void showDialog(){
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(MyApp());
