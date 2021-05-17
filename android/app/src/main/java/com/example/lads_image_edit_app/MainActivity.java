f5a69197104d06b781e5a0ff6d3749dc55519dc9
Mufazal <mufazalahmad2002@gmail.com>
2021-05-17 17:16:03 +0500

    nothing is done

--- android/app/src/main/java/com/example/lads_image_edit_app/MainActivity.java
+++ android/app/src/main/java/com/example/lads_image_edit_app/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.lads_image_edit_app;
 
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
+import androidx.annotation.WorkerThread;
+
+import org.jetbrains.annotations.NotNull;
+import org.tensorflow.lite.Interpreter;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.HashMap;
+import java.util.Map;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
 
+
 public class MainActivity extends FlutterActivity {
+
+    static {
+        System.loadLibrary("SuperResolution");
+    }
+
     private static final String TAG = "SuperResolution";
     private static final String MODEL_NAME = "ESRGAN.tflite";
     private static final int LR_IMAGE_HEIGHT = 50;
@@ -59,6 +81,9 @@ public class MainActivity extends FlutterActivity {
                         result.notImplemented();
                     }
                 });
+
+
+
     }
 
     String getImage(){
@@ -71,5 +96,45 @@ public class MainActivity extends FlutterActivity {
 
 
     }
+
+
+    @WorkerThread
+    public synchronized int[] doSuperResolution(int[] lowResRGB) {
+        return superResolutionFromJNI(superResolutionNativeHandle, lowResRGB);
+    }
+
+    private MappedByteBuffer loadModelFile() throws IOException {
+        try (AssetFileDescriptor fileDescriptor =
+                     AssetsUtils.getAssetFileDescriptorOrCached(getApplicationContext(), MODEL_NAME);
+             FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor())) {
+            FileChannel fileChannel = inputStream.getChannel();
+            long startOffset = fileDescriptor.getStartOffset();
+            long declaredLength = fileDescriptor.getDeclaredLength();
+            return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+        }
+    }
+
+    private void showToast(String str) {
+        Toast.makeText(getApplicationContext(), str, Toast.LENGTH_LONG).show();
+    }
+
+    private long initTFLiteInterpreter(boolean useGPU) {
+        try {
+            model = loadModelFile();
+        } catch (IOException e) {
+            Log.e(TAG, "Fail to load model", e);
+        }
+        return initWithByteBufferFromJNI(model, useGPU);
+    }
+
+    private void deinit() {
+        deinitFromJNI(superResolutionNativeHandle);
+    }
+
+    private native int[] superResolutionFromJNI(long superResolutionNativeHandle, int[] lowResRGB);
+
+    private native long initWithByteBufferFromJNI(MappedByteBuffer modelBuffer, boolean useGPU);
+
+    private native void deinitFromJNI(long superResolutionNativeHandle);
 }
 
